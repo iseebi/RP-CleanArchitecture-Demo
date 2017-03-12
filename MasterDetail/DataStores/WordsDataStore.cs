59a5f583bf874dcd7a58f46c4031bb9cbc9e0c0c
Nobuhiro Ito <iseebi@iseteki.net>
2017-03-13 01:39:23 +0900

    Add Realm Mobile Platform support

--- MasterDetail/DataStores/WordsDataStore.cs
+++ MasterDetail/DataStores/WordsDataStore.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using MasterDetail.Models;
 using Reactive.Bindings;
 using Realms;
+using Realms.Sync;
 
 namespace MasterDetail.DataStores
 {
@@ -16,19 +18,31 @@ namespace MasterDetail.DataStores
 
     public class WordsDataStore : IWordsDataStore
     {
-        public ReactiveProperty<List<Item>> Items { get; } = new ReactiveProperty<List<Item>>(new List<Item>
-        {
-            new Item { Title = "Sharp", Description = "C# is very sharp!"}
-        });
+        public ReactiveProperty<List<Item>> Items { get; } = new ReactiveProperty<List<Item>>(new List<Item>());
+
+        private User User { get; set; }
+        private SyncConfiguration SyncConfiguration { get; set; }
 
         public WordsDataStore()
         {
-            Reload();
+            ReloadAsync();
         }
 
-        private void Reload()
+        private async Task LoginAsync()
         {
-            using (var realm = Realm.GetInstance())
+            if (User == null)
+            {
+                var credentials = Credentials.UsernamePassword(AppCredentials.UserName, AppCredentials.Password, false);
+                User = await User.LoginAsync(credentials, new Uri(AppCredentials.AuthUrl));
+                Debug.WriteLine(User.Identity);
+                SyncConfiguration = new SyncConfiguration(User, new Uri(AppCredentials.DatabaseUrl));
+            }
+        }
+
+        private async Task ReloadAsync()
+        {
+            await LoginAsync();
+            using (var realm = Realm.GetInstance(SyncConfiguration))
             {
                 Items.Value = realm.All<RealmModels.Item>()
                     .ToList() // 一度 List<T> にしないと Select できない (Realm がサポートしていない)
@@ -39,16 +53,16 @@ namespace MasterDetail.DataStores
 
         public async Task AddItemAsync(Item item)
         {
-            using (var realm = Realm.GetInstance())
+            await LoginAsync();
+            using (var realm = Realm.GetInstance(SyncConfiguration))
             {
                 await realm.WriteAsync(r => r.Add(new RealmModels.Item
                     {
                         Title = item.Title,
                         Description = item.Description
-
                     })
                 );
-                Reload();
+                await ReloadAsync();
             }
         }
     }
